f1741f3e94250cd0eb7a7676903bff29dc25de03
sagar kondke <kondkesagar@gmail.com>
2026-03-17 15:56:45 +0530

    progress in project

--- db.py
+++ db.py
@@ -1,4 +1,6 @@
 from collections.abc import AsyncGenerator
+from sqlalchemy import Column, DateTime
+from datetime import datetime
 import uuid
 
 
@@ -9,13 +11,16 @@ from sqlalchemy.orm import DeclarativeBase, relationship
 
 DATABASE_URL='sqlite+aiosqlite:///./test.db'
 
-class post(DeclarativeBase):
-    __table__='post'
+class Base(DeclarativeBase):
+    pass
+
+class post(Base):
+    __tablename__='post'
 
     id= Column(UUID(as_uuid=True),primary_key=True,default=uuid.uuid4)
     caption =Column(Text)
     url =Column (String, nullable=False)
-    file_name= Column(DateTime, default=DateTime.utcnow)
+    file_name = Column(DateTime, default=datetime.utcnow)
 
 engine =create_async_engine(DATABASE_URL)
 async_session_maker =async_sessionmaker(engine,expire_on_commit=False)
@@ -23,7 +28,7 @@ async_session_maker =async_sessionmaker(engine,expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_aync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_async_session() ->AsyncGenerator[AsyncSession,None]:
